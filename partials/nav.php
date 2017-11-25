ea09de77261838280e41a12246b978d01677c234
JN646 <josh.ginn@hotmail.co.uk>
2017-11-25 01:52:38 +0000

    Tidy Nav Code
    Tidied the nav code and indents.

--- partials/nav.php
+++ partials/nav.php
@@ -2,19 +2,29 @@
 	<h3>LINKS</h3>
 	<h5>Jobs</h5>
 	<ul class="nav flex-column">
-		<li class="nav-item"><a class="nav-link" href="http://localhost/reten/jobs/index.php">List</a></li>
+		<li class="nav-item">
+			<a class="nav-link" href="http://localhost/reten/jobs/index.php">List</a>
+		</li>
 	</ul>
 	<h5>Inbox & Tasks</h5>
 	<ul class="nav flex-column">
-		<li class="nav-item"><a class="nav-link" href="http://localhost/reten/inbox/mail.php">Inbox</a></li>
-		<li class="nav-item"><a class="nav-link" href="http://localhost/reten/inbox/todo.php">Todo</a></li>
+		<li class="nav-item">
+			<a class="nav-link" href="http://localhost/reten/inbox/mail.php">Inbox</a>
+		</li>
+		<li class="nav-item">
+			<a class="nav-link" href="http://localhost/reten/inbox/todo.php">Todo</a>
+		</li>
 	</ul>
 	<h5>Reports</h5>
 	<ul class="nav flex-column">
-		<li class="nav-item"><a class="nav-link" href="#">Export</a></li>
+		<li class="nav-item">
+			<a class="nav-link" href="#">Export</a>
+		</li>
 	</ul>
 	<h5>User</h5>
 	<ul class="nav flex-column">
-		<li class="nav-item"><a class="nav-link" href="http://localhost/reten/dashboard/dashboard.php">Profile</a></li>
+		<li class="nav-item">
+			<a class="nav-link" href="http://localhost/reten/dashboard/dashboard.php">Profile</a>
+		</li>
 	</ul>
 </div>
