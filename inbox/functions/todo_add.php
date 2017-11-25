b711e8b2227724e6925b26ef72471457a839ef76
JN646 <josh.ginn@hotmail.co.uk>
2017-11-25 20:04:23 +0000

    Working Todo section
    + Working todo task setting.
    + Compose database link.

--- inbox/functions/todo_add.php
+++ inbox/functions/todo_add.php
@@ -1,4 +1,28 @@
 <?php
-//DB Config
-include("../../config/DBConfig.php");
+// Include config file
+require_once($_SERVER["DOCUMENT_ROOT"] . "/reten/config/DBconfig.php");
+
+if ($_POST['todo'] == 'Submit') {
+	//Escape user inputs for security
+	$todo_job = mysqli_real_escape_string($mysqli, $_REQUEST['todo_job']);
+	$todo_recipient = mysqli_real_escape_string($mysqli, $_REQUEST['todo_recipient']);
+	$todo_priority = mysqli_real_escape_string($mysqli, $_REQUEST['todo_priority']);
+	
+	//Attempt insert query execution
+	$sql = "INSERT INTO todo (todo_job, todo_recipient, todo_priority) VALUES ('$todo_job', '$todo_recipient', '$todo_priority')";
+}
+
+if(mysqli_query($mysqli, $sql)){
+	echo "<br>";
+	echo "<div class='container'>";
+	echo "<h1>Thank you!</h1>";
+    echo "<p>Records added successfully.</p>";
+	echo "<p><a href='../todo.php'>Back</a></p>";
+	echo "</div>";
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($mysqli);
+}
+ 
+// close connection
+mysqli_close($mysqli);
 ?>
--- inbox/todo_compose.php
+++ inbox/todo_compose.php
@@ -23,17 +23,17 @@ if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
 			<?php include($_SERVER["DOCUMENT_ROOT"] . "/reten/partials/nav.php"); ?>
 			<div class="col-md-11">
 				<h1 class="display-4">Compose Todo</h1>
-				<form>
+				<form action="functions/todo_add.php" method="post">
 					<div class="form-group">
-						<label for="formGroupExampleInput">To:</label> <input class="form-control" id="formGroupExampleInput" placeholder="Recipients" type="text">
+						<label for="todo_recipient">To:</label> <input class="form-control" name="todo_recipient" id="todo_recipient" placeholder="Recipients" type="text">
 					</div>
 					<div class="form-group">
-						<label for="formGroupExampleInput2">Job:</label> <input class="form-control" id="formGroupExampleInput2" placeholder="Job" type="text">
+						<label for="todo_job">Job:</label> <input class="form-control" name="todo_job" id="todo_job" placeholder="Job" type="text">
 					</div>
 					<div class="form-group">
-						<label for="formGroupExampleInput2">Priority:</label> <input class="form-control" id="formGroupExampleInput2" placeholder="Priority" type="text">
+						<label for="todo_priority">Priority:</label> <input class="form-control" name="todo_priority" id="todo_priority" placeholder="Priority" type="text">
 					</div>
-					<button class="btn btn-primary" type="submit">Send</button>
+					<button class="btn btn-primary" name="todo" value="Submit" type="submit">Send</button>
 				</form>
 			</div>
 		</div>
