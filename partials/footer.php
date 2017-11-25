22b8b21496ae936808e9d04188f7dc4aeae9290f
JN646 <josh.ginn@hotmail.co.uk>
2017-11-25 01:42:35 +0000

    Update Footer
    New footer links. Moved the help link to the base of the application.

--- partials/footer.php
+++ partials/footer.php
@@ -1,4 +1,5 @@
 <footer>
 <hr>
-  <small><p style="text-align: center">RMS System, build: 171125 for Oates by J Ginn</a>.</p></small>
+	<small><p style="text-align: center"><a href="http://localhost/reten/static/help.php">Help</a></p></small>
+	<small><p style="text-align: center">RMS System, build: 171125 for Oates by J Ginn.</p></small>
 </footer>
