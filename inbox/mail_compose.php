75b5f9dda202726a41ac02aa626da225f0263f84
JN646 <josh.ginn@hotmail.co.uk>
2017-11-25 00:48:34 +0000

    Add Mail Compose

---
+++ inbox/mail_compose.php
@@ -0,0 +1,28 @@
+<?php
+// Include config file
+require_once '../config/DBconfig.php';
+
+// Load Header
+include("../partials/header.php");
+?>
+<head>
+	<title>Compose</title>
+</head>
+<body>
+    <div class="container-fluid">
+		<div class="col-md-12">
+			<div class="row">
+				<?php include("../partials/nav.php"); ?>
+				<div class="col-md-11">
+					<h1 class="display-4">Compose</h1>
+						<div class="alert alert-warning" role="alert">
+							Feature partially implemented.
+						</div>
+						
+				</div>
+			</div>
+		</div>
+    </div>   
+<?php include("../partials/footer.php"); ?>	
+</body>
+</html>
