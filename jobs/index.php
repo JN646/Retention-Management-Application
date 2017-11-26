067be98200ca7c96ee3c78f960f5e58be5a5e37a
JN646 <josh.ginn@hotmail.co.uk>
2017-11-26 23:29:53 +0000

    Colour Groups
    + Colour groups to jobs.
    + Code for search and filtering.
    + Bug Fixes.

--- jobs/index.php
+++ jobs/index.php
@@ -33,9 +33,6 @@ if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
 								<a class="nav-link" href="<?php echo $environment; ?>jobs/add_contact.php">Add Job</a>
 							</li>
 							<li class="nav-item">
-								<a class="nav-link" href="#">Refresh</a>
-							</li>
-							<li class="nav-item">
 								<a class="nav-link" href="#">Request Help</a>
 							</li>
 						</ul>
@@ -43,16 +40,16 @@ if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
 					<div class="col-md-3 border border-success">
 						<h5>Filters</h5>
 						<div class="form-check form-check-inline">
-							<label class="form-check-label"><input class="form-check-input" id="inlineCheckbox1" type="checkbox" value="option1"> Purple</label>
+							<label class="form-check-label"><input class="form-check-input" id="inlineCheckbox1" onkeyup="myFunction()" type="checkbox" value="Purple">Purple</label>
 						</div>
 						<div class="form-check form-check-inline">
-							<label class="form-check-label"><input class="form-check-input" id="inlineCheckbox2" type="checkbox" value="option2"> Red</label>
+							<label class="form-check-label"><input class="form-check-input" id="inlineCheckbox2" type="checkbox" value="Red">Red</label>
 						</div>
 						<div class="form-check form-check-inline">
-							<label class="form-check-label"><input class="form-check-input" id="inlineCheckbox3" type="checkbox" value="option3"> Yellow</label>
+							<label class="form-check-label"><input class="form-check-input" id="inlineCheckbox3" type="checkbox" value="Yellow">Yellow</label>
 						</div>
 						<div class="form-check form-check-inline">
-							<label class="form-check-label"><input class="form-check-input" id="inlineCheckbox2" type="checkbox" value="option4"> Green</label>
+							<label class="form-check-label"><input class="form-check-input" id="inlineCheckbox2" type="checkbox" value="Green">Green</label>
 						</div>
 					</div>
 				</div><br>
@@ -69,7 +66,8 @@ if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
 				                echo "<th class='text-center' onclick='sortTable(3)'>Age</th>";
 				                echo "<th class='text-center' onclick='sortTable(4)'>Policy Provider</th>";
 				                echo "<th class='text-center' onclick='sortTable(5)'>Policy Number</th>";
-				                echo "<th class='text-center' onclick='sortTable(6)'>Policy Worth</th>";
+				                echo "<th class='text-center' onclick='sortTable(6)'>Group</th>";
+				                echo "<th class='text-center' onclick='sortTable(7)'>Policy Worth</th>";
 				                echo "<th class='text-center' style='width: 32px'>Notes</th>";
 				                echo "<th class='text-center' style='width: 32px'>Full</th>";
 				                echo "<th class='text-center' style='width: 32px'>Process</th>";
@@ -82,6 +80,24 @@ if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
 				                echo "<td class='text-center'>" . $row['client_age'] . "</td>";
 				                echo "<td>" . $row['client_provider'] . "</td>";
 				                echo "<td>" . $row['client_policynum'] . "</td>";
+								
+								// Colour coding groups.
+								if($row['client_group'] == 'GREEN'){
+									echo "<td class='text-center' style='background-color: #d9ffcc; color: #d9ffcc'><p>" . $row['client_group'] . "</p></td>";									
+								}
+								else if($row['client_group'] == 'RED'){
+									echo "<td class='text-center' style='background-color: #ffcccc; color: #ffcccc'><p>" . $row['client_group'] . "</p></td>";											
+								}
+								else if($row['client_group'] == 'YELLOW'){
+									echo "<td class='text-center' style='background-color: #ffffcc; color: #ffffcc'><p>" . $row['client_group'] . "</p></td>";											
+								}
+								else if($row['client_group'] == 'PURPLE'){
+									echo "<td class='text-center' style='background-color: #e6ccff; color: #e6ccff'><p>" . $row['client_group'] . "</p></td>";											
+								}
+								else {
+									echo "<td class='text-center'><p>" . $row['client_group'] . "</p></td>";
+								}
+								
 				                echo "<td class='text-center'><p>£" . $row['client_policyworth'] . "</p></td>";
 				                echo "<td class='text-center'><a href=#".$row['client_id']." data-toggle='modal' data-target='#exampleModal'><img src='../img/writing.png' alt='Notes' width='32' height='32'></a></td>";
 				                echo "<td class='text-center'><a href=#".$row['client_id']."><img src='../img/profile.png' alt='Profile' width='32' height='32'></a></td>";
@@ -97,10 +113,30 @@ if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
 				} else{
 				    echo "ERROR: Could not able to execute $sql. " . mysqli_error($mysqli);
 				}
-
+				
 				// Close connection
 				mysqli_close($mysqli);
 				?>
+				<script>
+					function myFunction() {
+						var input, filter, ul, li, a, i;
+						input = document.getElementById("myInput");
+						filter = input.value.toUpperCase();
+						ul = document.getElementById("table_search");
+						li = ul.getElementsByTagName("li");
+						for (i = 0; i < li.length; i++) {
+							a = li[i].getElementsByTagName("a")[6];
+							if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+								li[i].style.display = "";
+							} else {
+								li[i].style.display = "none";
+
+							}
+						}
+					}
+				</script>
+				
+				
 				<!-- Modal -->
 				<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
 				  <div class="modal-dialog" role="document">
