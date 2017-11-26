1fc6e2f141a84012378d515859458d80d1ff824c
JN646 <josh.ginn@hotmail.co.uk>
2017-11-26 17:42:27 +0000

    Nav Link Fixes
    + Used envrionment tags to make absolute links.

--- partials/nav.php
+++ partials/nav.php
@@ -3,37 +3,37 @@
 	<h5>Jobs</h5>
 	<ul class="nav flex-column">
 		<li class="nav-item">
-			<a class="nav-link" href="http://localhost/reten/jobs/index.php">List</a>
+			<a class="nav-link" href="<?php echo $environment; ?>jobs/index.php">List</a>
 		</li>
 	</ul>
 	<h5>Inbox & Tasks</h5>
 	<ul class="nav flex-column">
 		<li class="nav-item">
-			<a class="nav-link" href="http://localhost/reten/inbox/mail.php">Inbox</a>
+			<a class="nav-link" href="<?php echo $environment; ?>inbox/mail.php">Inbox</a>
 		</li>
 		<li class="nav-item">
-			<a class="nav-link" href="http://localhost/reten/inbox/todo.php">Todo</a>
+			<a class="nav-link" href="<?php echo $environment; ?>inbox/todo.php">Todo</a>
 		</li>
 	</ul>
 	<h5>Reports</h5>
 	<ul class="nav flex-column">
 		<li class="nav-item">
-			<a class="nav-link" href="#">Export</a>
+			<a class="nav-link" href=""<?php echo $environment; ?>export/index.php"">Export</a>
 		</li>
 	</ul>
 	<h5>User</h5>
 	<ul class="nav flex-column">
 		<li class="nav-item">
-			<a class="nav-link" href="http://localhost/reten/dashboard/dashboard.php">Dash</a>
-			<a class="nav-link" href="http://localhost/reten/rota/rota.php">Rota</a>
+			<a class="nav-link" href="<?php echo $environment; ?>dashboard/dashboard.php">Dash</a>
+			<a class="nav-link" href="<?php echo $environment; ?>rota/rota.php">Rota</a>
 		</li>
 	</ul>
 	<hr>
 	<h5>Admin</h5>
 	<ul class="nav flex-column">
 		<li class="nav-item">
-			<a class="nav-link" href="http://localhost/reten/admin/users.php">Users</a>
-			<a class="nav-link" href="http://localhost/reten/admin/clients.php">Clients</a>
+			<a class="nav-link" href="<?php echo $environment; ?>admin/users.php">Users</a>
+			<a class="nav-link" href="<?php echo $environment; ?>admin/clients.php">Clients</a>
 		</li>
 	</ul>
 </div>
