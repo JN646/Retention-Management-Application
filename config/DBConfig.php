f3fd743bd4b7aed1422391957d5552073f9f8541
JN646 <josh.ginn@hotmail.co.uk>
2017-12-02 14:31:55 +0000

    DB Config tweaks
    + Annotations.

--- config/DBConfig.php
+++ config/DBConfig.php
@@ -1,8 +1,10 @@
  <?php
+//Customise this file to match your server configutation.
 //MySQL connection
+//Server, Username, Password, Database.
 $mysqli = new mysqli('localhost', 'root', '', 'reten');
  
- //If connection fail
+//If connection fail
 if ($mysqli->connect_error) {
     die('Connect Error (' . $mysqli->connect_errno . ') '
             . $mysqli->connect_error);
