f85829f89a56d1c4249576917165a6eadf311f31
JN646 <josh.ginn@hotmail.co.uk>
2017-12-02 14:45:46 +0000

    Updated Global Variables
    + Annotations.

--- config/DBVar.php
+++ config/DBVar.php
@@ -1,4 +1,5 @@
 <?php
+//Global Variables.
 define("LOCAL", "http://localhost/reten/"); //local URL
 define("WEB", "http://foo.bar"); //website URL
 $environment = LOCAL; //change to WEB if you're live
