817617780075a464d4202aeb3e2ba9f174762aae
JN646 <josh.ginn@hotmail.co.uk>
2017-12-02 22:52:36 +0000

    Remove blank lines
    - Remove blank lines.

--- jobs/index.php
+++ jobs/index.php
@@ -1,13 +1,10 @@
 <?php
 // Include config file
 require_once($_SERVER["DOCUMENT_ROOT"] . "/reten/config/DBconfig.php");
-
 // Load Header
 include($_SERVER["DOCUMENT_ROOT"] . "/reten/partials/header.php");
-
 // Initialize the session
 session_start();
-
 // If session variable is not set it will redirect to login page
 if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
   header("location: http://localhost/reten/admin/login.php");
@@ -91,41 +88,30 @@ if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
 								else if($row['client_group'] == 'PURPLE'){
 									echo "<td class='text-center' style='background-color: #e6ccff; color: #e6ccff'; width: 5px></td>";
 								}
-								
 								// Client profile.
 				                echo "<td class='text-center'><a href=#".$row['client_id']."><img src='../img/profile.png' alt='Profile' width='24' height='24'></a></td>";
-								
 								// Combined first and last name fields.
 				                echo "<td>(Title) " . $row['client_fname'] . " " . $row['client_lname'] . "</td>";
-
 								// Client age.
 								// TODO Check NULL
 				                echo "<td class='text-center'>" . $row['client_age'] . "</td>";
-								
 								// Client address.
 								// TODO Check NULL
 				                echo "<td>34 Address Street, Town, County, XX000XX</td>";
-
 								// Client phone.
 								// TODO Check NULL
 				                echo "<td>07700000000</td>";
-								
 								// Client email.
 								// TODO Check NULL
 				                echo "<td>MyEmail@DomainName.co.uk</td>";
-
 								// Client provider.
 				                echo "<td>" . $row['client_provider'] . "</td>";
-
 								// Client policy number.
 				                echo "<td>" . $row['client_policynum'] . "</td>";
-
 								// Client policy worth.
 				                echo "<td class='text-center'><p>£" . $row['client_policyworth'] . "</p></td>";
-
 								// Client notes.
 				                echo "<td class='text-center'><a href=#".$row['client_id']." data-toggle='modal' data-target='#exampleModal'><img src='../img/writing.png' alt='Notes' width='24' height='24'></a></td>";
-
 								// Client process.
 				                echo "<td class='text-center'><a href=#".$row['client_id']."><img src='../img/flag.png' alt='Process' width='24' height='24'></a></td>";
 				            echo "</tr>";
@@ -139,7 +125,6 @@ if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
 				} else{
 				    echo "ERROR: Could not able to execute $sql. " . mysqli_error($mysqli);
 				}
-
 				// Close connection
 				mysqli_close($mysqli);
 				?>
@@ -168,18 +153,15 @@ if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
 					position: "right",			// Set positioning.
 					push: false,				// Disable content push.
 					});
-
 					// Search and filter data.
 					function myFunction() {
 						// declare variables.
 						var input, filter, ul, li, a, i;
-						
 						// set values to variables.
 						input = document.getElementById("myInput"); // search field ID.
 						filter = input.value.toUpperCase();
 						ul = document.getElementById("table_search"); // table ID.
 						li = ul.getElementsByTagName("li");
-						
 						// Search loop.
 						for (i = 0; i < li.length; i++) {
 							a = li[i].getElementsByTagName("a")[6]; // set the column to search by.
@@ -187,7 +169,6 @@ if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
 								li[i].style.display = "";
 							} else {
 								li[i].style.display = "none"; // remove bullets.
-
 							}
 						}
 					}
