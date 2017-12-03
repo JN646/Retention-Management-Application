c10690dc02cdb60f3407c7621c74d0bbc1c65197
JN646 <josh.ginn@hotmail.co.uk>
2017-12-03 00:37:15 +0000

    Header update
    - Remove blank spaces.
    + Condensed lines.

--- jobs/functions/discounts.php
+++ jobs/functions/discounts.php
@@ -1,4 +1,11 @@
 <?php
+require_once($_SERVER["DOCUMENT_ROOT"] . "/reten/config/DBconfig.php");		// Include config file
+//include($_SERVER["DOCUMENT_ROOT"] . "/reten/partials/header.php");			// Load Header
+session_start();															// Initialise the session
+if(!isset($_SESSION['username']) || empty($_SESSION['username'])){			// If session variable is not set it will redirect to login page
+  header("location: http://localhost/reten/admin/login.php");
+  exit;
+
 //Sets the level of discounts that can be offered based on the type of user account. Fail safe system to prevent 100% discounts.
 //Percent or Fixed
 $mode = "Fixed";
