68a6de7d1dbfe609b04ac0e791cc8637268a611b
JN646 <josh.ginn@hotmail.co.uk>
2017-12-03 00:46:01 +0000

    Header Tidy

--- inbox/todo_compose.php
+++ inbox/todo_compose.php
@@ -1,12 +1,8 @@
 <?php
-// Include config file
-require_once($_SERVER["DOCUMENT_ROOT"] . "/reten/config/DBconfig.php");
-// Load Header
-include($_SERVER["DOCUMENT_ROOT"] . "/reten/partials/header.php");
-// Initialize the session
-session_start();
-// If session variable is not set it will redirect to login page
-if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
+require_once($_SERVER["DOCUMENT_ROOT"] . "/reten/config/DBconfig.php");		// Include config file
+include($_SERVER["DOCUMENT_ROOT"] . "/reten/partials/header.php");			// Load Header
+session_start();															// Initialise the session
+if(!isset($_SESSION['username']) || empty($_SESSION['username'])){			// If session variable is not set it will redirect to login page
   header("location: http://localhost/reten/admin/login.php");
   exit;
 }
