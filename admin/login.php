fcb9e0ff574ef860b9f3d6aa107024622b5a9ad1
JN646 <josh.ginn@hotmail.co.uk>
2017-12-10 11:01:40 +0000

    Login Containers

--- admin/login.php
+++ admin/login.php
@@ -68,25 +68,28 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     $mysqli->close();
 }
 ?>
+<head><title>Login</title></head>
 <body>
-    <div class="container">
-        <h2>Login</h2>
-        <p>Please fill in your credentials to login.</p>
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-            <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
-                <label>Username:<sup>*</sup></label>
-                <input type="text" name="username"class="form-control" value="<?php echo $username; ?>">
-                <span class="help-block"><?php echo $username_err; ?></span>
-            </div>
-            <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
-                <label>Password:<sup>*</sup></label>
-                <input type="password" name="password" class="form-control">
-                <span class="help-block"><?php echo $password_err; ?></span>
-            </div>
-            <div class="form-group">
-                <input type="submit" class="btn btn-primary" value="Submit">
-            </div>
-        </form>
-    </div>
+    <div class="fluid-container">
+		<div class="col-md-4 offset-4">
+			<h2>Login</h2>
+			<p>Please fill in your credentials to login.</p>
+			<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+				<div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+					<label>Username:<sup>*</sup></label>
+					<input type="text" name="username"class="form-control" value="<?php echo $username; ?>">
+					<span class="help-block"><?php echo $username_err; ?></span>
+				</div>
+				<div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+					<label>Password:<sup>*</sup></label>
+					<input type="password" name="password" class="form-control">
+					<span class="help-block"><?php echo $password_err; ?></span>
+				</div>
+				<div class="form-group">
+					<input type="submit" class="btn btn-primary" value="Submit">
+				</div>
+			</form>
+		</div>
+	</div>
 <?php include($_SERVER["DOCUMENT_ROOT"] . "/reten/partials/footer.php"); ?></body>
 </html>
