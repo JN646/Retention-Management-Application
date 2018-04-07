d97db45f93cd0212e3536151af6ba33ac6f98300
JN646 <josh.ginn@hotmail.co.uk>
2018-04-07 13:29:17 +0100

    Dashboard Changes.

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -8,7 +8,7 @@ require_once($_SERVER["DOCUMENT_ROOT"] . "/reten/config/DBconfig.php");		// Incl
 include($_SERVER["DOCUMENT_ROOT"] . "/reten/partials/header.php");			// Load Header
 session_start();															// Initialise the session
 if(!isset($_SESSION['username']) || empty($_SESSION['username'])){			// If session variable is not set it will redirect to login page
-	header("location: http://localhost/reten/admin/login.php");
+	header("location: http://localhost/djx/djx/accounts/login.php");
 	exit;
 }
 
